ce5047c7537e9ef024068a9ff503603676ba2fff
Michael Harris <mah6@g.clemson.edu>
2023-04-10 22:57:24 +0000

    Created processInfo()

--- program.py
+++ program.py
@@ -24,8 +24,8 @@ def mainMenu():
     userChoice = input("Enter a menu option (1-7): ")
 
     if userChoice == "1":
-
         hardwareInfo()
+        pass
     elif userChoice == "2":
 
         pass
@@ -36,7 +36,7 @@ def mainMenu():
 
         pass
     elif userChoice == "5":
-
+        processInfo()
         pass
     elif userChoice == "6":
 
@@ -54,6 +54,7 @@ def mainMenu():
 #hardwareInfo(): 
 def hardwareInfo():
 
+
     #print hardware information
     print("CPU: " + platform.processor())
     print("CPU Cores: " + str(psutil.cpu_count(logical = False)) + " physical, " + str(psutil.cpu_count()) + " total")
@@ -69,6 +70,74 @@ def hardwareInfo():
 def systemPerformance():
 
     pass  
+
+def processInfo():
+    print( )
+    print("Process Information Menu:")
+    print("Select an option to continue:")
+
+    #menu choices
+    print("1) Show Current Process IDs")
+    print("2) Show information about a specific PID")
+    print("3) Show children processes of a specific PID")
+    print("4) Show parent processes of a specific PID")
+    print("5) Show current status of a specific PID")
+    print("10) Quit Program")
+
+    #prompt user input
+    userChoice = input("Enter a menu option (1-7): ")
+    if userChoice == "1":
+        print( )
+        print("Current Processes:")
+        print(str(psutil.pids()))
+        print( )
+        pass
+    elif userChoice == "2":
+        print( )
+        print("Current Processes:")
+        print(str(psutil.pids()))
+        print( )
+        userPID = input("Enter the PID to search for: ")
+        print(str(psutil.Process(int(userPID))))
+        pass
+    elif userChoice == "3":
+        print( )
+        print("Current Processes:")
+        print(str(psutil.pids()))
+        print( )
+        userPID = input("Enter the PID to search for children: ")
+        p = psutil.Process(int(userPID))
+        print(str(p.children(recursive = True)))
+        pass
+    elif userChoice == "4":
+        print( )
+        print("Current Processes:")
+        print(str(psutil.pids()))
+        print( )
+        userPID = input("Enter the PID to search for parents: ")
+        p = psutil.Process(int(userPID))
+        print(str(p.parents()))
+        pass
+    elif userChoice == "5":
+        print( )
+        print("Current Processes:")
+        print(str(psutil.pids()))
+        print( )
+        userPID = input("Enter the PID to search for status:")
+        print(str(userPID.status()))
+        pass
+    elif userChoice == "6":
+
+        pass
+    elif userChoice == "10":
+        exit()
+    else:
+        print("Invalid choice. Please enter a menu option again.")
+
+    processInfo()
+
+    pass
+    
 
 #call mainMenu() to start program
 mainMenu()
